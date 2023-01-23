599c9cd21a03e8a42b5d7a64ee5066e3589141f8
Alle43221 <allemoroz14@gmail.com>
2023-01-23 21:08:31 +0200

    Reorganized files Added .ico and .svg Added NavigationBar Added SFTransRobotics.ttf

---
+++ src/components/navigationBar/NavigationBar.tsx
@@ -0,0 +1,12 @@
+import "./NavigationBar.css"
+
+function NavigationBar(){
+    return (
+        <div className="NavigationBar">
+            <h1 className="TeamName">Perpetuum Mobile</h1>
+            <div className="NavigationOptions"></div>
+        </div>
+    )
+}
+
+export default NavigationBar
