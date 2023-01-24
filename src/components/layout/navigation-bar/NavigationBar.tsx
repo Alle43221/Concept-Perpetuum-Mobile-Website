ce233dca2925f4ca46253373ca6e8def230fe4d2
AleXutzZu <davidalex_robert@yahoo.com>
2023-01-24 13:11:38 +0200

    Added NavLinks to the Layout

--- src/components/layout/navigation-bar/NavigationBar.tsx
+++ src/components/layout/navigation-bar/NavigationBar.tsx
@@ -1,15 +1,16 @@
 import "./NavigationBar.css"
+import {NavLink} from "react-router-dom";
 
 
-function NavigationBar(){
+function NavigationBar() {
     return (
         <div className="NavigationBar">
             <h1 className="TeamName">Perpetuum Mobile</h1>
             <div className="NavigationOptions">
-                <h1 className="active">Home</h1>
-                <h1>Events</h1>
-                <h1>Sponsors</h1>
-                <h1>Contact</h1>
+                <NavLink to={"/"}><h1>Home</h1></NavLink>
+                <NavLink to={"/events"}><h1>Events</h1></NavLink>
+                <NavLink to={"/sponsors"}><h1>Sponsors</h1></NavLink>
+                <NavLink to={"/contact"}><h1>Contact</h1></NavLink>
             </div>
         </div>
     )
