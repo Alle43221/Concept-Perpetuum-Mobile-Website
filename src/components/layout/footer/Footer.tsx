52218842916cd4f7482b3d1dadb00efbbdbc29d2
Alle43221 <allemoroz14@gmail.com>
2023-01-24 10:02:53 +0200

    Added copyright notice

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -11,6 +11,9 @@ function Footer() {
                 <a href="https://www.printables.com/social/273911-perpetuum-mobile-065/"><img alt="" src="/printablesicon.png"/></a>
                 <a href="mailto:perpetuum.mobile@eminescusm.ro"><img alt="" src="/emailicon.png"/></a>
             </div>
+            <h1>
+                Copyright© 2022-2023 Perpetuum Mobile. All rights reserved. Made with 🖤 by <a  href="https://github.com/AleXutzZu">Alex David</a> and <a  href="https://github.com/Alle43221">Alexandra Moroz</a>.
+            </h1>
         </div>
     )
 }
