aad075da243cb5ea56cbcb2422ebb8dfcedd4b25
Alle43221 <allemoroz14@gmail.com>
2023-01-24 10:02:53 +0200

    Partially implemented the footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,9 +8,12 @@ function Footer(){
                 <a  href="https://www.instagram.com/perpetuummobileftc/"><img alt="" src="/instagramicon.png"></img></a>
                 <a  href=""><img alt="" src="/discordicon.png"></img></a>
                 <a  href=""><img alt="" src="/githubicon.png"></img></a>
-                <a  href=""><img alt="" src="/printablesicon.png"></img></a>
+                <a  href=""><img className="PrintablesIcon" alt="" src="/printablesicon.png"></img></a>
                 <a  href=""><img alt="" src="/emailicon.png"></img></a>
             </div>
+            <h1>
+                Copyright© 2022-2023 Perpetuum Mobile. All rights reserved. Made with 🖤 by Alex David and Alexandra Moroz.
+            </h1>
         </div>
     )
 }
