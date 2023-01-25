3705ba2b4aa084b3c4a5406d5741435b622ac360
Alle43221 <allemoroz14@gmail.com>
2023-01-25 14:54:05 +0200

    Started the work on the Home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -6,10 +6,10 @@ function  Home(){
             <div className={"Section1"}>
             <img className="Logo" alt="" src="/logoechipa.svg"></img>
             <div className={"Slideshow"}>
-                <img alt="" src={"/pictures/image2.jpg"} className={""}></img>
+                <img alt="" src={"/pictures/image1.jpg"} className={""}></img>
                 <button className="prev" >&#10094;</button>
                 <button className="next" >&#10095; </button>
-                <div>
+                <div className={"DotSpan"}>
                     <span className="dot"></span>
                     <span className="dot"></span>
                     <span className="dot"></span>
